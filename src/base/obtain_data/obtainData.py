c77940270192d53f6ed674570a988c4b9631329a
liudezhong <1922202995@qq.com>
2018-04-18 23:23:39 +0800

    方法抽取

--- src/base/obtain_data/obtainData.py
+++ src/base/obtain_data/obtainData.py
@@ -21,16 +21,20 @@ def obtainDataFromUrl(export, type, code):
     response = requests.get(downFileUrl, headers=UrlEnum.Url.send_headers.value)
 
     with open(filePathName, 'w', encoding='utf-8') as f1:
-        y = response.content.decode('utf8')
+        y = response.content.decode()
         z = y.replace('{\\', '{').replace('\\"', '"').replace('\\\\', '\\').replace('\"{', '{').replace('}\"', '}')
         f1.write(z)
-        print(z)
+        a = json.loads(z)
+        print(a)
 
+# 从同花顺获取信息并存入相应的表格中等待处理
+def generatorOriginalFiles(code):
+    # 利润表
+    obtainDataFromUrl('benefit', 'year', code)
+    # 现金流量表
+    obtainDataFromUrl('cash', 'year', code)
+    # 资产负债表
+    obtainDataFromUrl('debt', 'year', code)
 
 if __name__ =='__main__':
-    #利润表
-    obtainDataFromUrl('benefit', 'year', '002024')
-    #现金流量表
-    obtainDataFromUrl('cash', 'year', '002024')
-    #资产负债表
-    obtainDataFromUrl('debt', 'year', '002024')
+    generatorOriginalFiles('002024')
