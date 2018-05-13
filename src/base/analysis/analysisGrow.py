a40f1ddbfacc791532a1393e7b04e6b52c798182
liudezhong <liudezhong@finupgroup.com>
2018-05-13 08:18:54 +0800

    结构分析整理

---
+++ src/base/analysis/analysisGrow.py
@@ -0,0 +1,6 @@
+# -*- coding: UTF-8 -*-
+
+import pandas as pd
+import numpy as np
+
+# 从经济学的角度去分析
---
+++ src/base/analysis/plot.py
@@ -0,0 +1,10 @@
+# -*- coding: UTF-8 -*-
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(0, 2 * np.pi, 50)
+plt.plot(x, np.sin(x)) # 如果没有第一个参数 x，图形的 x 坐标默认为数组的索引
+plt.show() # 显示图形
+
+
